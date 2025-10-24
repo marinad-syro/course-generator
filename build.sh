35b59d8f81e8036823e4847f75e0d13985f8dfa6
marinad <marinaordukhanyan3@gmail.com>
2025-10-24 20:53:29 +0100

    updated build.sh to fix an error

--- build.sh
+++ build.sh
@@ -1,7 +1,17 @@
+#!/usr/bin/env bash
 set -o errexit
+set -o nounset
+set -o pipefail
 
-pip install -r requirements.txt
+# make pip stable & fresh
+python -m pip install --upgrade pip
 
+# install psycopg v3 binary first (pin a known good release)
+python -m pip install --no-cache-dir "psycopg[binary]==3.2.11"
+
+# then install the rest of your project's deps
+python -m pip install --no-cache-dir -r requirements.txt
+
+# collect static and run migrations
 python manage.py collectstatic --no-input
-
 python manage.py migrate
