95ab0c3920eec5cba2b5c3c82d2246a13304ee94
marinad <marinaordukhanyan3@gmail.com>
2025-10-24 21:44:55 +0100

    second try with superuser

--- build.sh
+++ build.sh
@@ -16,7 +16,33 @@ python -m pip install --no-cache-dir -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-if [[$CREATE_SUPERUSER]];
-then
-    python manage.py createsuperuser --no-input
+if [ "${CREATE_SUPERUSER:-}" = "true" ]; then
+  echo ">>> Creating/Updating Django superuser (build time)"
+  python - <<'PY'
+import os
+import django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yourproject.settings")  # <- replace yourproject
+django.setup()
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "")
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+if not username or not password:
+    print("DJANGO_SUPERUSER_USERNAME or DJANGO_SUPERUSER_PASSWORD not set; skipping superuser creation.")
+else:
+    qs = User.objects.filter(username=username)
+    if qs.exists():
+        u = qs.first()
+        u.set_password(password)
+        u.is_staff = True
+        u.is_superuser = True
+        u.save()
+        print(f"Updated password for existing superuser: {username}")
+    else:
+        User.objects.create_superuser(username=username, email=email, password=password)
+        print(f"Created superuser: {username}")
+PY
 fi
